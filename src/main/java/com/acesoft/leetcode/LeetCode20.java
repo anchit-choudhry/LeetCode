cf86decb3f9e63011965f542393a5ed1458ab011
Anchit <5533940+anchit-choudhry@users.noreply.github.com>
2021-05-30 14:01:34 -0400

    1. Additional problems solution 2. Updated Artifact Id and Name in pom.xml 3. Refactored and formatted code

---
+++ src/main/java/com/acesoft/leetcode/LeetCode20.java
@@ -0,0 +1,53 @@
+package com.acesoft.leetcode;
+
+import java.util.Stack;
+
+/*
+ * https://leetcode.com/problems/valid-parentheses
+ * 
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if
+ * the input string is valid.
+ * 
+ * An input string is valid if:
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * 
+ * Example 1:
+ * Input: s = "()"
+ * Output: true
+ * 
+ * Example 2:
+ * Input: s = "()[]{}"
+ * Output: true
+ * 
+ * Example 3:
+ * Input: s = "(]"
+ * Output: false
+ * 
+ * Example 4:
+ * Input: s = "([)]"
+ * Output: false
+ * 
+ * Example 5:
+ * Input: s = "{[]}"
+ * Output: true
+ * 
+ * Constraints:
+ * 1 <= s.length <= 10^4
+ * s consists of parentheses only '()[]{}'.
+ */
+public class LeetCode20 {
+
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else if (st.empty() || (ch == ')' && st.pop() != '(') || (ch == '}' && st.pop() != '{')
+                    || (ch == ']' && st.pop() != '[')) {
+                return false;
+            }
+        }
+        return st.empty();
+    }
+}
--- src/main/java/com/acesoft/leetcode/Leetcode70.java
+++ src/main/java/com/acesoft/leetcode/Leetcode70.java
@@ -30,9 +30,6 @@ public class Leetcode70 {
     int[] cache = new int[46];
 
     public int climbStairs(int n) {
-        if (n < 0) {
-            return 0;
-        }
         if (n < 2) {
             return 1;
         }
